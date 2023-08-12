c9d537cca3ed583b1193c651900655e9cd99e5dd
Zaid <zaid.khalaf2004au@gmail.com>
2023-08-12 15:52:15 +0800

    exception handeling

--- Prac2.py
+++ Prac2.py
@@ -9,6 +9,7 @@ print("5.Greatest common denominator recursion")
 print("6.Number conversions")
 user_input = int(input("please enter the number you want:"))
 
+
 if user_input == 1:
 # factorial iterative
     def calcNFactorial(n): 
@@ -16,10 +17,16 @@ if user_input == 1:
         for ii in range(n, 1, -1):
             nFactorial = nFactorial * ii
         return nFactorial
-    n = int(input("please enter a number:"))
-    # not sure if they want the user to enter the input or it already being initalised.
-    print(calcNFactorial(n))
-    # n = 1558
+    checking_input = True
+    while checking_input:
+        try: 
+            n = int(input("please enter a number:"))
+            result = (calcNFactorial(n))
+            print("the factorial of", n, "is", result)
+            checking_input = False
+        except ValueError:
+            print("Invalid input, please enter a valid number: ") 
+        # n = 1558
 
 elif user_input == 2:
 # # factorial recursive 
@@ -28,17 +35,21 @@ elif user_input == 2:
             return 1
         else:
             return n * calcNFactorialRecursive(n-1)
-    n = int(input("please enter a number:"))
-
-    result = calcNFactorialRecursive(n)
-    print(result)
-    # the highest i can go is 998, n = 998
+    checking_input2 = True
+    while checking_input2:
+        try:
+            n = int(input("please enter a number:"))
+            result = calcNFactorialRecursive(n)
+            print(result)
+            checking_input2 = False
+        except ValueError:
+            print("Invalid input, please try again: ")
+            # the highest i can go is 998, n = 998
 
 elif user_input == 3:
     # fibonacci recursive
     def fibRecursive(n):
         fibVal = 0
-
         if(n == 0):
             fibVal = 0
         elif(n == 1):
@@ -46,9 +57,15 @@ elif user_input == 3:
         else:
             fibVal = fibRecursive(n-1) + fibRecursive(n-2)
         return(fibVal)
-    n = int(input("please enter any number you want:"))
-    result = fibRecursive(n)
-    print(result)
+    checking_input_3 = True
+    while checking_input_3:
+        try:
+            n = int(input("please enter any number you want:"))
+            result = fibRecursive(n)
+            print(result)
+            checking_input_3 = False
+        except ValueError:
+            print("Invalid input, please try again: ")
 
 elif user_input == 4:
 # iterative fibonacci
@@ -67,9 +84,15 @@ elif user_input == 4:
                 lastVal = currVal
                 currVal = fibVal
         return fibVal
-    n = int(input("please enter a number:"))
-    result = fibIterative(n)
-    print(result)
+    checking_input_4 = True
+    while checking_input_4:
+        try:
+            n = int(input("please enter a number:"))
+            result = fibIterative(n)
+            print(result)
+            checking_input_4 = False
+        except ValueError:
+            print("invalid input, please try again: ")
 
 # greatest common denominator
 elif user_input == 5:
@@ -78,13 +101,16 @@ elif user_input == 5:
             return a
         else:
             return gcd_recursive(b, a % b)
-
-    
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
-
-    gcd = gcd_recursive(num1, num2)
-    print("GCD of", num1, "and", num2, "is", gcd)
+    checking_input_5 = True
+    while checking_input_5:
+        try:
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            gcd = gcd_recursive(num1, num2)
+            print("GCD of", num1, "and", num2, "is", gcd)
+            checking_input_5 = False
+        except ValueError:
+            print("Invalid input, please try again: ")
 # source = chatgpt openAI https://chat.openai.com/
 
 # number conversions
