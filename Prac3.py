e25ce5c5298500e326f935371c21995655292c1f
Zaid <zaid.khalaf2004au@gmail.com>
2023-08-19 11:18:08 +0800

    erasing the code

--- Prac3.py
+++
@@ -1,9 +0,0 @@
-#بسم الله الرحمن الرحيم
-import numpy as np
-
-#DSA stack
-class DSA_stack:
-    def __init__(self, size = 100):
-    #constructors
-        self.stack = np.array([" "] * size, dtype = object)
-        
