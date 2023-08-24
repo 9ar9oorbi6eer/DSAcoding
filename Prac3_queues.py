c12af3a3fd0e9a00018913eb8203f7971ed90632
Zaid <zaid.khalaf2004au@gmail.com>
2023-08-24 10:43:36 +0800

    implementing shuffling queue

--- Prac3_queues.py
+++ Prac3_queues.py
@@ -1,7 +1,36 @@
-#DSA queue
+import numpy as np
+
+# DSA queue
 class DSA_queue:
-    def __init__(self, size = 100):
-    # constructors
-        self.queue = np.array([" "] * size, dtype = object) 
+    def __init__(self, size=100):
+        # constructors
+        self.queue = np.array([" "] * size, dtype=object)
         self.count = 0
-    #getters
+    
+    # getters
+    def getCount(self):  
+        return self.count
+    
+    def isEmpty(self):
+        return self.count == 0
+    
+    def isFull(self, size):  
+        return self.count == size
+    
+    def peek(self):
+        if not self.isEmpty():
+            return self.queue[0]
+    
+    # setters
+    def enqueue(self, value):
+        if not self.isFull(len(self.queue)):
+            self.queue[self.count] = value
+            self.count += 1
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            front_value = self.queue[0]
+            self.queue[:-1] = self.queue[1:]
+            self.queue[-1] = " "
+            self.count -= 1
+            return front_value
