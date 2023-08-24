e0bd66a83eeeaa5235d0329b8b91c3071ef282e7
Zaid <zaid.khalaf2004au@gmail.com>
2023-08-24 14:52:54 +0800

    fixing some stuff

--- Equation_solver.py
+++ Equation_solver.py
@@ -2,5 +2,12 @@ from Prac3_equation_solver import EquationSolver
 
 equation_solver = EquationSolver()
 equation = input("Please enter an equation to solve: ")
+
+print("Input Infix Equation:", equation)
+
+postfix = equation_solver._parseInfixToPostfix(equation)
+postfix_expression = " ".join(str(token) for token in postfix.queue)
+print("Converted Postfix Expression:", postfix_expression)
+
 result = equation_solver.solve(equation)
-print(result)
+print("Result:", result)
