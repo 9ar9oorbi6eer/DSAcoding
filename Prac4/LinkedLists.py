16c0607304c47646b5bf6e066b92bc0938fbde67
Zaid <zaid.khalaf2004au@gmail.com>
2023-08-30 16:15:16 +0800

    creating test harness

--- Prac4/LinkedLists.py
+++ Prac4/LinkedLists.py
@@ -37,7 +37,7 @@ class DSALinkedList:
     # getters and setters
     def insertFirst(self, newValue):
         newNd = DSAListNode(newValue)
-        if DSALinkedList.isEmpty():
+        if self.isEmpty():
             self.head = newNd
         else:
             newNd.setNext(self.head)
@@ -67,12 +67,14 @@ class DSALinkedList:
     
     def removeLast(self):
         if self.isEmpty():
-            raise ValueError("list is empty")
+            raise ValueError("List is empty")
+        elif self.head == self.tail:  
+            self.head = None
+            self.tail = None
         else:
             self.tail = self.tail.getPrev()
-            self.tail.setNext(None)
-            if self.tail == None:
-                self.head == None
+            if self.tail:
+                self.tail.setNext(None)
                 
     def peekFirst(self):
         if self.isEmpty():
@@ -86,7 +88,63 @@ class DSALinkedList:
             raise ValueError("List is empty")
         else:
             return self.tail.getValue()
+
+class TestHarness:
+    def __init__(self):
+        self.linkedList = DSALinkedList()
+
+    def display_list(self):
+        current = self.linkedList.head
+        while current is not None:
+            print(current.getValue(), end=" -> ")
+            current = current.getNext()
+        print("None")
+
+    def run(self):
+        while True:
+            print("\nOptions:")
+            print("1. Insert First")
+            print("2. Insert Last")
+            print("3. Remove First")
+            print("4. Remove Last")
+            print("5. Display List")
+            print("6. Quit")
+
+            choice = input("Enter your choice: ")
+
+            if choice == '1':
+                value = input("Enter value to insert: ")
+                self.linkedList.insertFirst(value)
+            elif choice == '2':
+                value = input("Enter value to insert: ")
+                self.linkedList.insertLast(value)
+            elif choice == '3':
+                try:
+                    self.linkedList.removeFirst()
+                except ValueError as e:
+                    print(e)
+            elif choice == '4':
+                try:
+                    self.linkedList.removeLast()
+                except ValueError as e:
+                    print(e)
+            elif choice == '5':
+                self.display_list()
+            elif choice == '6':
+                print("Goodbye!")
+                break
+            else:
+                print("Invalid choice. Please select a valid option.")
+
+
+
+if __name__ == "__main__":
+    harness = TestHarness()
+    harness.run()
+
+
         
+
 
         
         
